ec31e97b9523c3cb7f3c70d001c572ec0f3cc354
onisley <onisley0424@gmail.com>
2022-03-07 22:15:44 +0900

    init: 프로젝트 생성

---
+++ keycat/src/main/java/com/example/keycat/KeycatApplication.java
@@ -0,0 +1,13 @@
+package com.example.keycat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycatApplication.class, args);
+	}
+
+}
---
+++ keycat/src/test/java/com/example/keycat/KeycatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keycat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
