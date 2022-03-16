0378d14c435b44fce94dc8f72d76e87f66ff384f
onisley <onisley0424@gmail.com>
2022-03-16 20:55:54 +0900

    feat: 연관관계 편의 메소드 추가

--- keycat/src/main/java/com/example/keycat/domain/Order.java
+++ keycat/src/main/java/com/example/keycat/domain/Order.java
@@ -32,4 +32,19 @@ public class Order {
     private LocalDateTime orderDate;
 
     private OrderStatus status;
+
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        this.orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
